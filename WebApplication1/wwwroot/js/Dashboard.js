143e11498e59174fb5dccc82c085f1f62dcc542f
sumit kumar Maurya <mauryasumit440@gmail.com>
2022-12-05 19:22:04 +0530

    Excel Upload Reports Added to dashboard

---
+++ WebApplication1/wwwroot/js/Dashboard.js
@@ -0,0 +1,100 @@
+﻿
+$(function () {
+    $.ajax({
+        type: "GET",
+        url: "/Dashboard/GetUploadHistory?id=1",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccess,
+        failure: function (response) {
+            alert(response.d);
+        },
+        error: function (response) {
+            alert(response.d);
+        }
+    });
+
+    $('#ReportFiletype').change(function () {
+
+        var filetypeCode = $('#ReportFiletype').val()
+
+        $.ajax({
+            type: "GET",
+            url: "/Dashboard/GetUploadHistory?id=" + filetypeCode,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: OnSuccess,
+            
+            failure: function (response) {
+                alert(response.d);
+            },
+            error: function (response) {
+                alert(response.d);
+            }
+        })
+    })
+})
+function OnSuccess(response) {
+    //var records = JSON.parse(response);
+    console.log(response);
+    
+    var totalRow = [];
+    var uploadDate = new Array();
+    for (records of response) {
+        totalRow.push(records.numberOfRows);
+        uploadDate.push(records.date);
+    }
+    var options = {
+        series: [{
+            name:"Total Uploded rows",
+            data: totalRow
+        }],
+        chart: {
+            type: 'bar',
+            height: 350,
+           
+        },
+        plotOptions: {
+            bar: {
+                borderRadius: 4,
+                horizontal: false,
+                
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        xaxis: {
+            type: 'datetime',
+            categories: uploadDate,
+            labels: {
+                datetimeFormatter: {
+                    year: 'yyyy',
+                    month: 'MMM \'yy',
+                    day: 'dd MMM',
+                    hour: 'HH:mm'
+                }
+            },
+            title: {
+                text: 'Uploaded Date'
+            }
+        },
+        yaxis: {
+            title: {
+                text: 'Number of Uploaded Rows'
+            }
+        },
+        noData: {
+            text: 'No Records...'
+        },
+        tooltip: {
+            x: {
+                format: 'dd/MM/yy HH:mm'
+            },
+        }
+    };
+    $('#chart').empty();
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+
+}
