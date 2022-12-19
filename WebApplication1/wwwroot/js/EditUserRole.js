a0a342f5171a6e0f1bfc8e0ac0a415a04c07a1c5
sumit kumar Maurya <mauryasumit440@gmail.com>
2022-12-19 15:33:10 +0530

    sort icon fixed

--- WebApplication1/wwwroot/js/EditUserRole.js
+++ WebApplication1/wwwroot/js/EditUserRole.js
@@ -99,6 +99,7 @@ function OnSuccess(response) {
                 { data: 'email'},
                 {
                     data: null,
+                    orderable: false,
                     "mRender": function (data, type, full) {
                         return '<a class="btn btn-warning btn-sm mx-4" href="/AccountView/EditUserRoleDetails?id=' + data.uId + '&currentRole=' + data.role + '" ><i class="bi bi-pencil-fill" title="Edit"></i></a><button id="delete" class="btn btn-danger btn-sm DatatableDeleteBtn "  value="' + data.uId +'" ><i class="bi bi-trash-fill" title="Delete"></i></button> ';
 
